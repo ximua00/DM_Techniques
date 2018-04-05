5523c115212244b30224213ea6e40c8597cfbea0
ximua00 <kishanarendra@gmail.com>
2018-04-05 16:48:33 +0200

    Added time variables

--- Assignment_1/adv/Ass1_Syntax/ReadData.R
+++ Assignment_1/adv/Ass1_Syntax/ReadData.R
@@ -1,5 +1,5 @@
 ######################################
-#Install packages if not available
+#Install packages, if not available
 install.packages("data.table")
 install.packages("lubridate")
 ######################################
@@ -41,10 +41,38 @@ newdata <- reshape(Mood,
 
 
 ######################################
-#Define variable as "time" format
+#Define variable as "time" format - full date and time
+newdata$converted.time <- as_datetime(newdata$time)
 
+#Define date variable
+newdata$date <- as_date(newdata$time)
+
+#Define hour variable (hour of the day)
+newdata$hour <- hour(newdata$converted.time)
+
+#Use hour of the day to define time of day
+newdata$time.of.day[newdata$hour <= 6] <- "dawn"
+newdata$time.of.day[newdata$hour > 6 & newdata$hour <= 12] <- "morning"
+newdata$time.of.day[newdata$hour > 12 & newdata$hour <= 18] <- "afternoon"
+newdata$time.of.day[newdata$hour > 18 & newdata$hour <= 24] <- "evening"
+######################################
+
+
+######################################
+#Sort by id and date
+newdata <- newdata[order(id, date),]
+######################################
 
 
 
 ######################################
-
+#EXPLORATORY ANALYSIS
+######################################
+#for how many days each "subject" is tracked
+DaysPerID <- c()
+for (id_ in unique(newdata$id)) {
+  n_days <- length(unique(newdata$date[which(newdata$id == id_)]))
+  new_id <- c(id_, n_days)
+  DaysPerID <- rbind(DaysPerID, new_id, deparse.level = 0)
+}
+######################################
