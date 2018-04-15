4a34bddd2faff92b1dad2c9a1d86fcbd516473be
Kabur <tomasfabry1@gmail.com>
2018-04-15 23:25:54 +0200

    interpolation, discarding vars and adding meanlags

--- Assignment_1/adv/Ass1_Syntax/ReadData.R
+++ Assignment_1/adv/Ass1_Syntax/ReadData.R
@@ -76,3 +76,170 @@ newdata <- newdata[order(id, converted.time),]
 min_day <- as.numeric(min(newdata$date))
 newdata$nth_day <- as.numeric(unlist(lapply(X = as.numeric(newdata$date), FUN = function(x) x+1-min_day)))
 rm(min_day)
+
+######################################
+
+
+
+
+
+# PREVIOUS AggregateData.R script:
+#AGGREGATE DATA (DAILY LEVEL)
+#make sure that newdata is loaded in environment - from ReadData.R
+
+#This syntax: 
+# - creates interesting variables
+# - aggregates the data
+# - prepares the data for modelling
+######################################
+
+
+######################################
+#Install packages, if not available
+#install.packages("zoo")
+######################################
+
+######################################
+#Load package
+library(zoo)
+######################################
+
+
+
+######################################
+# Create variables number of times each
+# application was open throughout the day (use the sum in aggregation to get this value). 
+newdata$count_value.appCat.builtin <- ifelse(is.na(newdata$value.appCat.builtin) == FALSE,
+                                             newdata$count_value.appCat.builtin <- 1,
+                                             newdata$count_value.appCat.builtin <- 0)
+
+newdata$count_value.appCat.communication <- ifelse(is.na(newdata$value.appCat.communication) == FALSE,
+                                                   newdata$count_value.appCat.communication <- 1,
+                                                   newdata$count_value.appCat.communication <- 0)
+
+newdata$count_value.appCat.entertainment <- ifelse(is.na(newdata$value.appCat.entertainment) == FALSE,
+                                                   newdata$count_value.appCat.entertainment <- 1,
+                                                   newdata$count_value.appCat.entertainment <- 0)
+
+newdata$count_value.appCat.finance <- ifelse(is.na(newdata$value.appCat.finance) == FALSE,
+                                             newdata$count_value.appCat.finance <- 1,
+                                             newdata$count_value.appCat.finance <- 0)
+
+newdata$count_value.appCat.game <- ifelse(is.na(newdata$value.appCat.game) == FALSE,
+                                          newdata$count_value.appCat.game <- 1,
+                                          newdata$count_value.appCat.game <- 0)
+
+newdata$count_value.appCat.office <- ifelse(is.na(newdata$value.appCat.office) == FALSE,
+                                            newdata$count_value.appCat.office <- 1,
+                                            newdata$count_value.appCat.office <- 0)
+
+newdata$count_value.appCat.other <- ifelse(is.na(newdata$value.appCat.other) == FALSE,
+                                           newdata$count_value.appCat.other <- 1,
+                                           newdata$count_value.appCat.other <- 0)
+
+newdata$count_value.appCat.social <- ifelse(is.na(newdata$value.appCat.social) == FALSE,
+                                            newdata$count_value.appCat.social <- 1,
+                                            newdata$count_value.appCat.social <- 0)
+
+newdata$count_value.appCat.travel <- ifelse(is.na(newdata$value.appCat.travel) == FALSE,
+                                            newdata$count_value.appCat.travel <- 1,
+                                            newdata$count_value.appCat.travel <- 0)
+
+newdata$count_value.appCat.unknown <- ifelse(is.na(newdata$value.appCat.unknown) == FALSE,
+                                             newdata$count_value.appCat.unknown <- 1,
+                                             newdata$count_value.appCat.unknown <- 0)
+
+newdata$count_value.appCat.utilities <- ifelse(is.na(newdata$value.appCat.utilities) == FALSE,
+                                               newdata$count_value.appCat.utilities <- 1,
+                                               newdata$count_value.appCat.utilities <- 0)
+
+newdata$count_value.appCat.weather <- ifelse(is.na(newdata$value.appCat.weather) == FALSE,
+                                             newdata$count_value.appCat.weather <- 1,
+                                             newdata$count_value.appCat.weather <- 0)
+
+
+######################################
+
+
+
+
+######################################
+# Aggregate data on by day and id. 
+aggData = newdata[, .(count = .N, 
+                      agg_mood = mean(value.mood, na.rm=TRUE),
+                      agg_arousal = mean(value.circumplex.arousal, na.rm=TRUE),
+                      agg_valence = mean(value.circumplex.valence, na.rm=TRUE),
+                      agg_activity = mean(value.activity, na.rm=TRUE),
+                      agg_screen = mean(value.screen, na.rm=TRUE),
+                      agg_call = sum(value.call, na.rm=TRUE),
+                      agg_sms = sum(value.sms, na.rm=TRUE),
+                      agg_builtin = mean(value.appCat.builtin, na.rm=TRUE),
+                      agg_communication = mean(value.appCat.communication, na.rm=TRUE),
+                      agg_entertainment = mean(value.appCat.entertainment, na.rm=TRUE),
+                      agg_finance = mean(value.appCat.finance, na.rm=TRUE),
+                      agg_game = mean(value.appCat.game, na.rm=TRUE),
+                      agg_office = mean(value.appCat.office, na.rm=TRUE),
+                      agg_other = mean(value.appCat.other, na.rm=TRUE),
+                      agg_social = mean(value.appCat.social, na.rm=TRUE),
+                      agg_travel = mean(value.appCat.travel, na.rm=TRUE),
+                      agg_unknown = mean(value.appCat.unknown, na.rm=TRUE),
+                      agg_utilities = mean(value.appCat.utilities, na.rm=TRUE),
+                      agg_weather = mean(value.appCat.weather, na.rm=TRUE), 
+                      agg_count_builtin = sum(count_value.appCat.builtin, na.rm=TRUE), 
+                      agg_count_communication = sum(count_value.appCat.communication, na.rm=TRUE), 
+                      agg_count_entertainment = sum(count_value.appCat.entertainment, na.rm=TRUE),
+                      agg_count_finance = sum(count_value.appCat.finance, na.rm=TRUE),
+                      agg_count_game = sum(count_value.appCat.game, na.rm=TRUE),
+                      agg_count_office = sum(count_value.appCat.office, na.rm=TRUE),
+                      agg_count_other = sum(count_value.appCat.other, na.rm=TRUE),
+                      agg_count_social = sum(count_value.appCat.social, na.rm=TRUE),
+                      agg_count_travel = sum(count_value.appCat.travel, na.rm=TRUE),
+                      agg_count_unknown = sum(count_value.appCat.unknown, na.rm=TRUE),
+                      agg_count_utilities = sum(count_value.appCat.utilities, na.rm=TRUE),
+                      agg_count_weather = sum(count_value.appCat.weather, na.rm=TRUE)),
+                  by = .(id, date)]
+
+######################################
+# Include other more interesting variables
+# Add weekday
+aggData$weekday <- weekdays(aggData$date)
+
+# Count number of different applications used per day
+aggData$open_count <- aggData[,apply(X = aggData[, 11:22], MARGIN = 1, FUN = function(x) sum(!is.nan(x)))]
+######################################
+
+
+
+######################################
+#Exclude data with missing Mood
+PreData <- subset(aggData, (!is.nan(aggData[, aggData$agg_mood]) | !is.nan(aggData[, aggData$agg_screen]) ))
+######################################
+
+
+######################################
+#Interpolate missing values for mood, arousal and valence
+PreData$interp_mood <- na.approx(PreData$agg_mood, na.rm = FALSE)
+PreData$interp_arousal <- na.approx(PreData$agg_arousal, na.rm = FALSE)
+PreData$interp_valence <- na.approx(PreData$agg_valence, na.rm = FALSE)
+
+#remove non interpolated variables
+PreData$agg_mood <- NULL
+PreData$agg_arousal <- NULL
+PreData$agg_valence <- NULL
+######################################
+
+
+######################################
+#Visualise correlations
+ggcorr(data = PreData, label = TRUE, label_alpha = TRUE, label_size = 2.3, label_round = 2, 
+       hjust = 0.9, size = 3)
+######################################
+
+
+######################################
+rm(Mood, aggData, newdata)
+######################################
+
+
+
+
---
+++ Assignment_1/adv/Ass1_Syntax/Regression_Preprocessing.R
@@ -0,0 +1,155 @@
+######################################
+# PREPROCESS DATA FOR MODELLING
+# (make sure that PreData is in environment - from AggregateData.R)
+
+#This syntax: 
+# - Deletes correlated variables
+# - deals with missing values
+# - makes Lags and Moving Averages Variables
+######################################
+
+
+######################################
+#Load helper functions
+source("./Helper_Functions.R")
+######################################
+
+pls = PreData
+
+######################################
+# Delete Highly correlated Variables
+# 1 - Delete count because no informative values
+PreData$count <- NULL
+
+# 2 - Delete either count_built_in or open count
+# Choose agg_count_builtin because 
+PreData$agg_builtin <- NULL
+
+#Visualise correlations after changes
+ggcorr(data = PreData, label = TRUE, label_alpha = TRUE, label_size = 2.3, label_round = 2, 
+       hjust = 0.9, size = 3)
+######################################
+
+######################################
+#Check Variable filling
+#Replace with 0's since we are dealing with time
+PreData[is.na(PreData)] <- 0
+######################################
+
+######################################
+# keep only statistically significant variables (according to a prior linear regression run)
+# NOTE: only saturday is relevant, but here we have all weekdays together, they are split up once PreData is a dataframe down below
+keeps = c("id", "date", "weekday", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office")
+PreData = PreData[, keeps, with = FALSE]
+######################################
+
+######################################
+#Make lag Variables
+# PreData$lag_mood = PreData[, .(lag_mood = shift(interp_mood)), by = id][, 2]
+# PreData$lag_arousal = PreData[, .(lag_arousal = shift(interp_arousal)), by = id][, 2]
+# PreData$lag_valence = PreData[, .(lag_valence = shift(interp_valence)), by = id][, 2]
+# PreData$lag_activity = PreData[, .(lag_activity = shift(agg_activity)), by = id][, 2]
+# PreData$lag_screen = PreData[, .(lag_screen = shift(agg_screen)), by = id][, 2]
+# PreData$lag_call = PreData[, .(lag_call = shift(agg_call)), by = id][, 2]
+# PreData$lag_sms = PreData[, .(lag_sms = shift(agg_sms)), by = id][, 2]
+# PreData$lag_communication = PreData[, .(lag_communication = shift(agg_communication)), by = id][, 2]
+# PreData$lag_entertainment = PreData[, .(lag_entertainment = shift(agg_entertainment)), by = id][, 2]
+# PreData$lag_finance = PreData[, .(lag_finance = shift(agg_finance)), by = id][, 2]
+# PreData$lag_game = PreData[, .(lag_game = shift(agg_game)), by = id][, 2]
+# PreData$lag_office = PreData[, .(lag_office = shift(agg_office)), by = id][, 2]
+# PreData$lag_other = PreData[, .(lag_other = shift(agg_other)), by = id][, 2]
+# PreData$lag_social = PreData[, .(lag_social = shift(agg_social)), by = id][, 2]
+# PreData$lag_travel = PreData[, .(lag_travel = shift(agg_travel)), by = id][, 2]
+# PreData$lag_unknown = PreData[, .(lag_unknown = shift(agg_unknown)), by = id][, 2]
+# PreData$lag_utilities = PreData[, .(lag_utilities = shift(agg_utilities)), by = id][, 2]
+# PreData$lag_weather = PreData[, .(lag_weather = shift(agg_weather)), by = id][, 2]
+# PreData$lag_count_builtin = PreData[, .(lag_count_builtin = shift(agg_count_builtin)), by = id][, 2]
+# PreData$lag_count_communication = PreData[, .(lag_count_communication = shift(agg_count_communication)), by = id][, 2]
+# PreData$lag_count_entertainment = PreData[, .(lag_count_entertainment = shift(agg_count_entertainment)), by = id][, 2]
+# PreData$lag_count_finance = PreData[, .(lag_count_finance = shift(agg_count_finance)), by = id][, 2]
+# PreData$lag_count_game = PreData[, .(lag_count_game = shift(agg_count_game)), by = id][, 2]
+# PreData$lag_count_office = PreData[, .(lag_count_office = shift(agg_count_office)), by = id][, 2]
+# PreData$lag_count_other = PreData[, .(lag_count_other = shift(agg_count_other)), by = id][, 2]
+# PreData$lag_count_social = PreData[, .(lag_count_social = shift(agg_count_social)), by = id][, 2]
+# PreData$lag_count_travel = PreData[, .(lag_count_travel = shift(agg_count_travel)), by = id][, 2]
+# PreData$lag_count_unknown = PreData[, .(lag_count_unknown = shift(agg_count_unknown)), by = id][, 2]
+# PreData$lag_count_utilities = PreData[, .(lag_count_utilities = shift(agg_count_utilities)), by = id][, 2]
+# PreData$lag_count_weather = PreData[, .(lag_count_weather = shift(agg_count_weather)), by = id][, 2]
+# PreData$lag_open_count = PreData[, .(lag_open_count = shift(open_count)), by = id][, 2]
+# 
+# 
+# #Delete unnecessary (current t) variables
+# PreData$agg_screen <- NULL
+# PreData$agg_communication <- NULL
+# PreData$agg_game <- NULL
+# PreData$agg_social <- NULL
+# PreData$agg_utilities <- NULL
+# PreData$agg_count_entertainment <- NULL
+# PreData$agg_count_office <- NULL
+# PreData$agg_count_travel <- NULL
+# PreData$agg_count_weather <- NULL
+# PreData$agg_call <- NULL
+# PreData$agg_entertainment <- NULL
+# PreData$agg_office <- NULL
+# PreData$agg_travel <- NULL
+# PreData$agg_weather <- NULL
+# PreData$agg_count_finance <- NULL
+# PreData$agg_count_other <- NULL
+# PreData$agg_count_unknown <- NULL
+# PreData$agg_sms <- NULL
+# PreData$agg_finance <- NULL
+# PreData$agg_other <- NULL
+# PreData$agg_unknown <- NULL
+# PreData$agg_count_communication <- NULL
+# PreData$agg_count_game <- NULL
+# PreData$agg_count_social <- NULL
+# PreData$agg_count_utilities <- NULL
+
+######################################
+
+
+
+######################################
+# Make Dummies for weekday
+PreData_df <- as.data.frame(PreData)
+for(level in unique(PreData_df$weekday)){
+  PreData_df[paste("dummy", level, sep = "_")] <- ifelse(PreData_df$weekday == level, 1, 0)
+}
+
+#Delete weekday
+PreData_df$weekday <- NULL
+
+#Delete one dummy to avoid multicollinearity
+PreData_df$dummy_Monday <- NULL
+######################################
+# discard all dummies except saturday, keep the rest of vars
+keeps = c("id", "date", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office", "dummy_Saturday")
+PreData_df = PreData_df[keeps]
+
+
+# ######################################
+# # create lag variables of given history(number of days to look back into the past)
+# histories = list(1, 2, 5, 7)
+# 
+# for(name in names(PreData_df)) {
+# 
+#   # do not create lag vars for these columns
+#   if(name == "id" | name == "date" | name == "count") {
+#     print(paste("Skipping", name, sep = " "))
+#     next
+#   }
+# 
+#   for(history in histories) {
+#     new_name = paste("meanlag", name, history, sep = "_")
+# 
+#     # TODO/NOTE: this line gives an error, don't know how to fix yet: 
+#     #   - if I use PreData_df it's "unused argument (by = id)"
+#     #   - if I use PreData it's some other nonsense >{
+#     PreData_df[paste(new_name)] = PreData_df[, .(new_name = shift(name, n = history)), by = id][, 2]
+#   }
+# }
+# ######################################
+
+
+
+
