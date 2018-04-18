725c2e6fb645036eb08c674516ed541bdebc778b
Kabur <tomasfabry1@gmail.com>
2018-04-18 18:51:54 +0200

    svm works, but exluding saturday column; see code for the error

--- Assignment_1/adv/Ass1_Syntax/Regression_Preprocessing.R
+++ Assignment_1/adv/Ass1_Syntax/Regression_Preprocessing.R
@@ -37,44 +37,44 @@ PreData[is.na(PreData)] <- 0
 
 ######################################
 # keep only statistically significant variables (according to a prior linear regression run)
-# NOTE: only saturday is relevant, but here we have all weekdays together, they are split up once PreData is a dataframe down below
-keeps = c("id", "date", "weekday", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office")
-PreData = PreData[, keeps, with = FALSE]
+# # NOTE: only saturday is relevant, but here we have all weekdays together, they are split up once PreData is a dataframe down below
+# keeps = c("id", "date", "weekday", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office")
+# PreData = PreData[, keeps, with = FALSE]
 ######################################
 
 ######################################
 #Make lag Variables
-# PreData$lag_mood = PreData[, .(lag_mood = shift(interp_mood)), by = id][, 2]
-# PreData$lag_arousal = PreData[, .(lag_arousal = shift(interp_arousal)), by = id][, 2]
-# PreData$lag_valence = PreData[, .(lag_valence = shift(interp_valence)), by = id][, 2]
-# PreData$lag_activity = PreData[, .(lag_activity = shift(agg_activity)), by = id][, 2]
-# PreData$lag_screen = PreData[, .(lag_screen = shift(agg_screen)), by = id][, 2]
-# PreData$lag_call = PreData[, .(lag_call = shift(agg_call)), by = id][, 2]
-# PreData$lag_sms = PreData[, .(lag_sms = shift(agg_sms)), by = id][, 2]
-# PreData$lag_communication = PreData[, .(lag_communication = shift(agg_communication)), by = id][, 2]
-# PreData$lag_entertainment = PreData[, .(lag_entertainment = shift(agg_entertainment)), by = id][, 2]
-# PreData$lag_finance = PreData[, .(lag_finance = shift(agg_finance)), by = id][, 2]
-# PreData$lag_game = PreData[, .(lag_game = shift(agg_game)), by = id][, 2]
-# PreData$lag_office = PreData[, .(lag_office = shift(agg_office)), by = id][, 2]
-# PreData$lag_other = PreData[, .(lag_other = shift(agg_other)), by = id][, 2]
-# PreData$lag_social = PreData[, .(lag_social = shift(agg_social)), by = id][, 2]
-# PreData$lag_travel = PreData[, .(lag_travel = shift(agg_travel)), by = id][, 2]
-# PreData$lag_unknown = PreData[, .(lag_unknown = shift(agg_unknown)), by = id][, 2]
-# PreData$lag_utilities = PreData[, .(lag_utilities = shift(agg_utilities)), by = id][, 2]
-# PreData$lag_weather = PreData[, .(lag_weather = shift(agg_weather)), by = id][, 2]
-# PreData$lag_count_builtin = PreData[, .(lag_count_builtin = shift(agg_count_builtin)), by = id][, 2]
-# PreData$lag_count_communication = PreData[, .(lag_count_communication = shift(agg_count_communication)), by = id][, 2]
-# PreData$lag_count_entertainment = PreData[, .(lag_count_entertainment = shift(agg_count_entertainment)), by = id][, 2]
-# PreData$lag_count_finance = PreData[, .(lag_count_finance = shift(agg_count_finance)), by = id][, 2]
-# PreData$lag_count_game = PreData[, .(lag_count_game = shift(agg_count_game)), by = id][, 2]
-# PreData$lag_count_office = PreData[, .(lag_count_office = shift(agg_count_office)), by = id][, 2]
-# PreData$lag_count_other = PreData[, .(lag_count_other = shift(agg_count_other)), by = id][, 2]
-# PreData$lag_count_social = PreData[, .(lag_count_social = shift(agg_count_social)), by = id][, 2]
-# PreData$lag_count_travel = PreData[, .(lag_count_travel = shift(agg_count_travel)), by = id][, 2]
-# PreData$lag_count_unknown = PreData[, .(lag_count_unknown = shift(agg_count_unknown)), by = id][, 2]
-# PreData$lag_count_utilities = PreData[, .(lag_count_utilities = shift(agg_count_utilities)), by = id][, 2]
-# PreData$lag_count_weather = PreData[, .(lag_count_weather = shift(agg_count_weather)), by = id][, 2]
-# PreData$lag_open_count = PreData[, .(lag_open_count = shift(open_count)), by = id][, 2]
+PreData$lag_mood = PreData[, .(lag_mood = shift(interp_mood)), by = id][, 2]
+PreData$lag_arousal = PreData[, .(lag_arousal = shift(interp_arousal)), by = id][, 2]
+PreData$lag_valence = PreData[, .(lag_valence = shift(interp_valence)), by = id][, 2]
+PreData$lag_activity = PreData[, .(lag_activity = shift(agg_activity)), by = id][, 2]
+PreData$lag_screen = PreData[, .(lag_screen = shift(agg_screen)), by = id][, 2]
+PreData$lag_call = PreData[, .(lag_call = shift(agg_call)), by = id][, 2]
+PreData$lag_sms = PreData[, .(lag_sms = shift(agg_sms)), by = id][, 2]
+PreData$lag_communication = PreData[, .(lag_communication = shift(agg_communication)), by = id][, 2]
+PreData$lag_entertainment = PreData[, .(lag_entertainment = shift(agg_entertainment)), by = id][, 2]
+PreData$lag_finance = PreData[, .(lag_finance = shift(agg_finance)), by = id][, 2]
+PreData$lag_game = PreData[, .(lag_game = shift(agg_game)), by = id][, 2]
+PreData$lag_office = PreData[, .(lag_office = shift(agg_office)), by = id][, 2]
+PreData$lag_other = PreData[, .(lag_other = shift(agg_other)), by = id][, 2]
+PreData$lag_social = PreData[, .(lag_social = shift(agg_social)), by = id][, 2]
+PreData$lag_travel = PreData[, .(lag_travel = shift(agg_travel)), by = id][, 2]
+PreData$lag_unknown = PreData[, .(lag_unknown = shift(agg_unknown)), by = id][, 2]
+PreData$lag_utilities = PreData[, .(lag_utilities = shift(agg_utilities)), by = id][, 2]
+PreData$lag_weather = PreData[, .(lag_weather = shift(agg_weather)), by = id][, 2]
+PreData$lag_count_builtin = PreData[, .(lag_count_builtin = shift(agg_count_builtin)), by = id][, 2]
+PreData$lag_count_communication = PreData[, .(lag_count_communication = shift(agg_count_communication)), by = id][, 2]
+PreData$lag_count_entertainment = PreData[, .(lag_count_entertainment = shift(agg_count_entertainment)), by = id][, 2]
+PreData$lag_count_finance = PreData[, .(lag_count_finance = shift(agg_count_finance)), by = id][, 2]
+PreData$lag_count_game = PreData[, .(lag_count_game = shift(agg_count_game)), by = id][, 2]
+PreData$lag_count_office = PreData[, .(lag_count_office = shift(agg_count_office)), by = id][, 2]
+PreData$lag_count_other = PreData[, .(lag_count_other = shift(agg_count_other)), by = id][, 2]
+PreData$lag_count_social = PreData[, .(lag_count_social = shift(agg_count_social)), by = id][, 2]
+PreData$lag_count_travel = PreData[, .(lag_count_travel = shift(agg_count_travel)), by = id][, 2]
+PreData$lag_count_unknown = PreData[, .(lag_count_unknown = shift(agg_count_unknown)), by = id][, 2]
+PreData$lag_count_utilities = PreData[, .(lag_count_utilities = shift(agg_count_utilities)), by = id][, 2]
+PreData$lag_count_weather = PreData[, .(lag_count_weather = shift(agg_count_weather)), by = id][, 2]
+PreData$lag_open_count = PreData[, .(lag_open_count = shift(open_count)), by = id][, 2]
 # 
 # 
 ######################################
@@ -89,59 +89,80 @@ source("./CreateMovingAverages.R")
 
 ######################################
 # #Delete unnecessary (current t) variables
-# PreData$agg_screen <- NULL
-# PreData$agg_communication <- NULL
-# PreData$agg_game <- NULL
-# PreData$agg_social <- NULL
-# PreData$agg_utilities <- NULL
-# PreData$agg_count_entertainment <- NULL
-# PreData$agg_count_office <- NULL
-# PreData$agg_count_travel <- NULL
-# PreData$agg_count_weather <- NULL
-# PreData$agg_call <- NULL
-# PreData$agg_entertainment <- NULL
-# PreData$agg_office <- NULL
-# PreData$agg_travel <- NULL
-# PreData$agg_weather <- NULL
-# PreData$agg_count_finance <- NULL
-# PreData$agg_count_other <- NULL
-# PreData$agg_count_unknown <- NULL
-# PreData$agg_sms <- NULL
-# PreData$agg_finance <- NULL
-# PreData$agg_other <- NULL
-# PreData$agg_unknown <- NULL
-# PreData$agg_count_communication <- NULL
-# PreData$agg_count_game <- NULL
-# PreData$agg_count_social <- NULL
-# PreData$agg_count_utilities <- NULL
-
+PreData$agg_screen <- NULL
+PreData$agg_communication <- NULL
+PreData$agg_game <- NULL
+PreData$agg_social <- NULL
+PreData$agg_utilities <- NULL
+PreData$agg_count_entertainment <- NULL
+PreData$agg_count_office <- NULL
+PreData$agg_count_travel <- NULL
+PreData$agg_count_weather <- NULL
+PreData$agg_call <- NULL
+PreData$agg_entertainment <- NULL
+PreData$agg_office <- NULL
+PreData$agg_travel <- NULL
+PreData$agg_weather <- NULL
+PreData$agg_count_finance <- NULL
+PreData$agg_count_other <- NULL
+PreData$agg_count_unknown <- NULL
+PreData$agg_sms <- NULL
+PreData$agg_finance <- NULL
+PreData$agg_other <- NULL
+PreData$agg_unknown <- NULL
+PreData$agg_count_communication <- NULL
+PreData$agg_count_game <- NULL
+PreData$agg_count_social <- NULL
+PreData$agg_count_utilities <- NULL
+PreData$agg_activity = NULL
+PreData$agg_count_builtin = NULL
+PreData$open_count = NULL
+PreData$interp_arousal = NULL
+PreData$interp_valence = NULL
 ######################################
 
 
 
-######################################
-# Make Dummies for weekday
-PreData_df <- as.data.frame(PreData)
-for(level in unique(PreData_df$weekday)){
-  PreData_df[paste("dummy", level, sep = "_")] <- ifelse(PreData_df$weekday == level, 1, 0)
+# ######################################
+# # Make Dummies for weekday
+# # PreData_df <- as.data.frame(PreData)
+# for(level in unique(PreData_df$weekday)){
+#   PreData[paste("dummy", level, sep = "_")] <- ifelse(PreData_df$weekday == level, 1, 0)
+# }
+# 
+# #Delete weekday
+# PreData_df$weekday <- NULL
+# 
+# #Delete one dummy to avoid multicollinearity
+# PreData_df$dummy_Monday <- NULL
+# ######################################
+
+##########################################
+# ALTERNATIVE TO DUMMIES - because they refuse to work for me 
+# convert saturdays to 1; all other days to 0
+func_saturday_only <- function (day) {
+  if(day == "Saturday")
+    return(1)
+  else
+    return(0)
+
 }
+PreData$saturday = lapply(PreData$weekday, func_saturday_only)
+PreData$weekday = NULL
+PreData$date = NULL # once we have weekday info, the date is useless right?
+# # for time series regression: lag saturday by 1 day: if the day we predict would be saturday, we want to tell it to the model by having the previous day marked with '1'
+# # PreData_df$lag_saturday <- PreData_df[,.(ma2 = as.numeric(get.mav(agg__count_office, n = 1))), by = id] [, 2]
+##########################################
 
-#Delete weekday
-PreData_df$weekday <- NULL
 
-#Delete one dummy to avoid multicollinearity
-PreData_df$dummy_Monday <- NULL
+
 ######################################
-
-
-
-######################################
-# doing an experiment on linear regression per user 
-experiment <- PreData_df
-
-# discard all dummies except saturday, keep the rest of vars
-keeps = c("id", "date", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office", "dummy_Saturday")
-PreData_df = PreData_df[keeps]
+# # doing an experiment on linear regression per user 
+# experiment <- PreData_df
+# 
+# # discard all dummies except saturday, keep the rest of vars
+# keeps = c("id", "date", "agg_activity", "interp_valence", "interp_mood", "interp_arousal", "agg_utilities", "agg_count_entertainment", "agg_count_office", "dummy_Saturday")
+# PreData_df = PreData_df[keeps]
 ######################################
 
 
--- Assignment_1/adv/Ass1_Syntax/toms_main.R
+++ Assignment_1/adv/Ass1_Syntax/toms_main.R
@@ -3,94 +3,61 @@ source("Regression_preprocessing.R")
 source("Helper_Functions.R")
 # source("CreateMovingAverages.R")
 
-PreData_df = as.data.frame(PreData)
 ##########################################
-# convert saturdays to 1; all other days to 0
-func_saturday_only <- function (day) {
-  if(day == "Saturday") 
-    return(1)
-  else 
-    return(0)
+# split data
+PreData$saturday = NULL # because I'm getting invalid type (list) for variable saturday all the time
+data = splitdata_id(PreData)
 
-}
-PreData_df$saturday = lapply(PreData_df$weekday, func_saturday_only)
-PreData_df$weekday = NULL
-##########################################
+train = data[[1]]
+valid = data[[2]]
+test = data[[3]]
 
-##########################################
-# add mav's of 2
-PreData_df$ma2_interp_mood<- PreData_df[,.(ma2 = as.numeric(get.mav(interp_mood, n = 2))), by = id] [, 2]
-PreData_df$ma2_interp_arousal<- PreData_df[,.(ma2 = as.numeric(get.mav(interp_arousal, n = 2))), by = id] [, 2]
-PreData_df$ma2_interp_valence<- PreData_df[,.(ma2 = as.numeric(get.mav(interp_valence, n = 2))), by = id] [, 2]
-PreData_df$ma2_agg_office<- PreData_df[,.(ma2 = as.numeric(get.mav(agg_office, n = 2))), by = id] [, 2]
-PreData_df$ma2_agg_activity <- PreData_df[,.(ma2 = as.numeric(get.mav(agg_activity, n = 2))), by = id] [, 2]
-PreData_df$ma2_agg_utilities<- PreData_df[,.(ma2 = as.numeric(get.mav(agg_utilities, n = 2))), by = id] [, 2]
-PreData_df$ma2_agg_count_entertainment <- PreData_df[,.(ma2 = as.numeric(get.mav(agg_count_entertainment, n = 2))), by = id] [, 2]
-PreData_df$ma2_agg_count_office <- PreData_df[,.(ma2 = as.numeric(get.mav(agg_count_office, n = 2))), by = id] [, 2]
-PreData_df$interp_arousal = NULL
-PreData_df$interp_valence = NULL
-PreData_df$agg_office = NULL
-PreData_df$agg_activity = NULL
-PreData_df$agg_utilities = NULL
-PreData_df$agg_count_entertainment = NULL
-PreData_df$agg_count_office = NULL
-##########################################
-
-##########################################
-# lag saturday by 1 day: if the day we predict would be saturday, we want to tell it to the model by having the previous day marked with '1'
-PreData_df$lag_saturday <- PreData_df[,.(ma2 = as.numeric(get.mav(agg__count_office, n = 1))), by = id] [, 2]
-##########################################
-
-##########################################
-# # split data
-# data = splitdata_id(PreData_df)
-# 
-# train = data[1]
-# valid = data[2]
-# test = data[3]
+train$id = NULL
+valid$id = NULL
+test$id = NULL
 ##########################################
 
 ##########################################
 # perform svm 
-PreData_df$date = NULL
-PreData_df$id = NULL
-PreData_df$saturday = NULL # because I'm getting invalid type (list) for variable saturday all the time
-
-# install.packages("e1071")
-library("e1071")
+library("e1071") # install.packages("e1071")
 
 main_var = c("interp_mood")
 
-# x = subset(PreData_df, select = !(names(PreData_df) %in% main_var))
-# y = subset(PreData_df, select = main_var)
-x = subset(PreData_df, select = -interp_mood)
-y = PreData_df$interp_mood
+train_x = subset(train, select = -interp_mood)
+train_y = train$interp_mood
+val_x = subset(valid, select = -interp_mood)
+val_y = valid$interp_mood
 
-svm_model = svm(interp_mood ~ ., data=PreData_df)
-# svm_model = svm(x, y)
-# svm_model = svm(y ~ x, PreData_df)
+svm_model = svm(interp_mood ~ ., data=train)
 summary(svm_model)
 
-pred = predict(svm_model, x)
-table(pred, y)
-err = pred - y
+pred = predict(svm_model, val_x)
+# table(pred, val_y)
+err = pred - val_y
 
-# is.na(err)
-cc = complete.cases(err)
-num_missing = nrow(err) - sum(pred)
-
-# mean(sqrt(err))
+mse = mean(err^2)
+perf = Performance(val_y, pred)
 ##########################################
 
 ##########################################
-# attach(iris)
-# x = subset(iris, select = -Species)
-# y = Species
-# svm_model = svm(Species ~ ., data=iris)
-# summary(svm_model)
-# pred = predict(svm_model, x)
-# table(pred, y)
+# optimizing svm - doesnt work yet
+
+# welp, apparently there is no direct way to measure variable importance in svm's
+#   - gotta do this with linear regression
+# library(caret) # install.packages("caret)
+# varImp(svm_model)
+# methods(varImp)
+
+# svm_tune = tune(svm_model, train.x = train_x, train.y = train_y,
+                # kernel = "radial", ranges = list(cost=10^(-1:2), gamma=c(.5, 1, 2)))
+                
+# svm_tune <- tune(svm, train.x=train_x, train.y=train_y, 
+#                  kernel="radial", ranges=list(cost=10^(-1:2), gamma=c(.5,1,2)))
+# print(svm_tune)
+
 ##########################################
+
+
 
 
 
