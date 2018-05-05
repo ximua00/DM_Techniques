323ee4e5dcb65514bf0928ebcfaad606ec07a2de
ximua00 <kishanarendra@gmail.com>
2018-05-06 00:18:08 +0200

    Benchmark model and Evaluation

---
+++ Assignment_2/Syntax/evaluate.py
@@ -0,0 +1,116 @@
+import numpy as np
+import time
+
+
+
+def dcg_at_k(r, k = 40, method=1):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> dcg_at_k(r, 1)
+    3.0
+    >>> dcg_at_k(r, 1, method=1)
+    3.0
+    >>> dcg_at_k(r, 2)
+    5.0
+    >>> dcg_at_k(r, 2, method=1)
+    4.2618595071429155
+    >>> dcg_at_k(r, 10)
+    9.6051177391888114
+    >>> dcg_at_k(r, 11)
+    9.6051177391888114
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Discounted cumulative gain
+    """
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+
+def ndcg_at_k(r, k = 40, method = 1):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> ndcg_at_k(r, 1)
+    1.0
+    >>> r = [2, 1, 2, 0]
+    >>> ndcg_at_k(r, 4)
+    0.9203032077642922
+    >>> ndcg_at_k(r, 4, method=1)
+    0.96519546960144276
+    >>> ndcg_at_k([0], 1)
+    0.0
+    >>> ndcg_at_k([1], 2)
+    1.0
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Normalized discounted cumulative gain
+    """
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
+
+
+
+def evaluate_ndcg(dataset, k = 38, method = 1):
+
+    #TODO: make this faster
+
+    verbosity_interval = 1000
+    start = time.time()
+
+    ndcg_array = np.array([])
+    for iteration, search_id in enumerate(dataset['srch_id'].unique()):
+
+        ndcg = ndcg_at_k(dataset['score'][dataset['srch_id'] == search_id])
+
+        if iteration % verbosity_interval == 0 and iteration != 0:
+            print("ITERATION %r, time=%.2fs" % (iteration, time.time() - start))
+            print (ndcg)
+
+        ndcg_array = np.append(ndcg_array, ndcg)
+
+    return np.mean(ndcg_array)
+
+######################
+# EXAMPLE
+# ranking = [1, 5, 0]
+
+# score = dcg_at_k(ranking)
+# print(score)
+
+# score = ndcg_at_k(ranking)
+# print (score)
+
+# ranking = [0, 0, 5, 1, 0]
+
+# score = dcg_at_k(ranking)
+# print(score)
+
+# score = ndcg_at_k(ranking)
+# print (score)
+
---
+++ Assignment_2/Syntax/rank_ReviewScore.py
@@ -0,0 +1,37 @@
+'''
+Benchmark rank according to review's score
+'''
+
+import pandas as pd
+import numpy as np
+import time
+import pickle
+import SplitData
+from evaluate import evaluate_ndcg as evaluate
+
+
+
+#Load training data
+#start = time.time()
+validation_data = pickle.load(open('../pickled_data/validation_data.pkl', 'rb'))
+#print ("Time to read data = %.2fs " % (time.time() - start))
+
+
+#check data filling
+print ('Number of missing values in column prop_review_score {} \n'.format(validation_data['prop_review_score'].isnull().sum()))
+
+#Replace missing with 0's
+validation_data['prop_review_score'].fillna(0, inplace = True)
+print ('Filled NAs {} \n'.format(validation_data['prop_review_score'].isnull().sum()))
+
+
+#Per query sort by prop_review_score
+validation_data.sort_values(by = ['srch_id', 'prop_review_score'], ascending=[1, 0], inplace = True)
+
+
+#Evaluate NDCG
+validation_data['score'] = validation_data['click_bool'] + 5*validation_data['booking_bool']
+validation_data['score'][validation_data['score'] > 5] = 5 
+
+ndcg = evaluate(validation_data)
+print (ndcg)
