ad1968af1b59be0f7d1db643012734214a6ec511
tharangni <tharangni@hotmail.com>
2018-05-19 14:10:25 +0200

    Delete rank_GBM.py

--- Assignment_2/Syntax/rank_GBM.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from sklearn.ensemble import GradientBoostingClassifier  #GBM algorithm
-from sklearn.model_selection import GridSearchCV, cross_validate   #Perforing grid search
-from sklearn import metrics #Additional scklearn functions
